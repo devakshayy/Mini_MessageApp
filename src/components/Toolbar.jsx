acefe5600164c56938c6a69a75af50de8186173b
devakshayy <devakshay916@gmail.com>
2024-12-05 21:05:00 +0530

    corrected and learned

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,30 +1,49 @@
-import React from 'react'
 
-const AlertButon = ({message,children}) => {
-   return(
-       <button
-           onClick={() => alert(message)}
-           className='border p-2 m-4 rounded-md'
-       >
-          {children}
-      </button>
-   )
+const Button = ({children,onClick}) => {
+    
+    return (
+        <button 
+           onClick={onClick}
+           className='border rounded-md p-2 m-4'>
+           {children}
+        </button>
+    )
+
 }
+
+const  PlayButton = ({movieName}) => {
+
+    function handlePlaybtn (){
+        alert(`Now playing the Video of ${movieName}`)
+    }
+    return (
+        <Button onClick={handlePlaybtn}>
+            Play {movieName}
+        </Button>
+    )
+}
+
+const UpLoadImage = () => {
+    return (
+        <Button onClick={() => alert('Uploading Image!!!')}>
+             Upload image
+        </Button>
+    )
+}
+
 
 const Toolbar = () => {
   return (
     <div>
-         <AlertButon message={"Now Playing the Movie"}>
-             Play Movie
-         </AlertButon>
-         <AlertButon message={"Uplaoding Image Started"}>
-             Upload Image
-         </AlertButon>
-         <AlertButon message={"Downloading Video Started"}>
-             Download Video
-         </AlertButon>
+        <h1 className="text-2xl pt-2 font-bold text-gray-500">Here im implement 
+            the different onClick btn with same btn</h1>
+      <PlayButton movieName='Avatar 2'/>
+      <UpLoadImage/>
     </div>
   )
 }
 
 export default Toolbar
+
+
+// Passing event handlers as props 
