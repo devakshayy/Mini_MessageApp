d07f3398e94e7dc19a1783b067dc061811ef212e
devakshayy <devakshay916@gmail.com>
2024-12-05 21:19:32 +0530

    all done with onSmash

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,9 +1,12 @@
 
-const Button = ({children,onClick}) => {
-    
+const Button = ({children,onSmash}) => {                  
+    //Build in components like <button> and <div> supports 'browser event names' like onClick
+    //When ever you're building your own component you can name their 'event handler props' any Way that you like
+    //By convention event handler prop should start with on 
+    //Here we use "onSmash" happy coding bro    
     return (
         <button 
-           onClick={onClick}
+           onClick={onSmash}
            className='border rounded-md p-2 m-4'>
            {children}
         </button>
@@ -17,7 +20,7 @@ const  PlayButton = ({movieName}) => {
         alert(`Now playing the Video of ${movieName}`)
     }
     return (
-        <Button onClick={handlePlaybtn}>
+        <Button onSmash={handlePlaybtn}>
             Play {movieName}
         </Button>
     )
@@ -25,7 +28,7 @@ const  PlayButton = ({movieName}) => {
 
 const UpLoadImage = () => {
     return (
-        <Button onClick={() => alert('Uploading Image!!!')}>
+        <Button onSmash={() => alert('Uploading Image!!!')}>
              Upload image
         </Button>
     )
@@ -35,8 +38,11 @@ const UpLoadImage = () => {
 const Toolbar = () => {
   return (
     <div>
-        <h1 className="text-2xl pt-2 font-bold text-gray-500">Here im implement 
-            the different onClick btn with same btn</h1>
+        <h1 className="text-2xl 
+                       pt-2 font-bold
+                     text-gray-500">
+                        Here im implement 
+                      the different onClick btn with same btn</h1>
       <PlayButton movieName='Avatar 2'/>
       <UpLoadImage/>
     </div>
